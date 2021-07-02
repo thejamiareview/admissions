91819f3c92399b257d39a4f678098b0227c6248d
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-07-02 16:29:49 +0530

    basic setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const ejs = require("ejs");
+
+// Handling environment variables
+require("dotenv").config();
+
+
+// Setting up initial express server
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+// Firing up (listening to) the exress server
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server is up on port ${port}`)
+});
