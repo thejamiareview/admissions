1cd5a13a81ec3cd43e0ec287840a18e24e41c2b6
Abdullah Ajaz Siddiqui <abdulajaz20@gmail.com>
2021-07-03 19:45:26 +0530

    refined it
    added a saperate css(Sass) styling sheet
    added a seperate html (ejs for under construction banner)

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("static"));
 
-// req and response
-app.get('/', (req, res) => {
-  res.render("main")
-})
+// set site under construction
+plumb = true
+if(plumb = true){
+  app.get('/', (req, res) => {
+    res.render("underConstruction")
+  })
+}
+
 
 
 // Firing up (listening to) the exress server
